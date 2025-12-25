2507f9ba19e6cd8bdf0055f56869e3d864b9f9af
biobios <biobios1024@gmail.com>
2025-12-25 16:14:45 +0900

    feat: add theorem for relational power set specification

--- src/ZF/ZF.v
+++ src/ZF/ZF.v
@@ -1061,6 +1061,16 @@ Module ZF (core:CoreZF).
         apply nature_memberOf.
         apply (memCons (B ^ A) (|_| <: f In (B ^ A) | (exists Pf, P (memCons (B ^ A) f Pf)) :>) H).
     Defined.
+
+    Theorem RelPowSpec : forall (A: setType) (R: setType -> Prop), <: x In A | R x :> In Pow A.
+    Proof.
+        intros.
+        apply PowerSet.
+        intro.
+        intro.
+        apply SchemaOfSpecification in H.
+        apply H.
+    Defined.
     
     Goal forall (A B:setType) (P:memberOf A->memberOf B->Prop), (forall a, exists! b, P a b) -> (exists! f:A → B, (forall a, P a (f←a))).
         intros.
